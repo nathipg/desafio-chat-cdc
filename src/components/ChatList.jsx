fc42667f2407d8826ba04b40bb04930cc2f9b4dc
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-01-30 16:25:48 -0300

    ChatList fit screen allowing scroll

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -6,6 +6,9 @@ const StyledChatList = styled.div`
   gap: 1rem;
   background: var(--pale-blue);
   padding: 1rem;
+  max-height: calc(100vh - 73px - 54px); // 73px => OptionsBar height; 54px => MessageBar height
+  min-height: calc(100vh - 73px - 54px); // 73px => OptionsBar height; 54px => MessageBar height
+  overflow: auto;
 `;
 
 const ChatList = ({ children }) => {
