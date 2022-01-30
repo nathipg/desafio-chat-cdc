b7a917b521213be35cf1f1b2010cce729e892c29
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-01-30 16:13:47 -0300

    List messages in chat page

--- src/containers/ChatPage.jsx
+++ src/containers/ChatPage.jsx
@@ -2,12 +2,29 @@ import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
+import ChatList from '../components/ChatList';
 import OptionsBar from '../components/OptionsBar';
 import OptionsBarItem from '../components/OptionsBarItem';
 import UserIcon from '../components/UserIcon';
+import ChatListItem from '../components/ChatListItem';
 
 const ChatPage = ({ receiver, loggedUser, messages }) => {
   const navigate = useNavigate();
+  const chatUsers = [receiver, loggedUser];
+
+  const listMessages = () => {
+    return messages.map((message, key) => {
+      const user = chatUsers.find(user => user.id === message.user);
+      return (
+        <ChatListItem
+          key={key}
+          user={user}
+          message={message.content}
+          side={user.id === loggedUser.id ? 'right' : 'left'}
+        />
+      );
+    });
+  };
 
   return (
     <>
@@ -20,6 +37,9 @@ const ChatPage = ({ receiver, loggedUser, messages }) => {
           <FontAwesomeIcon icon={faEllipsisV} />
         </OptionsBarItem>
       </OptionsBar>
+      <ChatList>
+        {listMessages()}
+      </ChatList>
     </>
   );
 };
