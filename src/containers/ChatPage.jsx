244d37749c4b065c260a32ff07ca2c52bda4705b
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-01-30 12:50:12 -0300

    Navigate from chat to inbox

--- src/containers/ChatPage.jsx
+++ src/containers/ChatPage.jsx
@@ -1,5 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+
+import OptionsBar from '../components/OptionsBar';
+import OptionsBarItem from '../components/OptionsBarItem';
+
 const ChatPage = () => {
-  return 'CHAT PAGE';
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <OptionsBar>
+        <OptionsBarItem align="left">
+          <FontAwesomeIcon icon={faArrowLeft} onClick={() => navigate('/')} />
+        </OptionsBarItem>
+        <OptionsBarItem>
+          <FontAwesomeIcon icon={faEllipsisV} />
+        </OptionsBarItem>
+      </OptionsBar>
+    </>
+  );
 };
 
 export default ChatPage;
